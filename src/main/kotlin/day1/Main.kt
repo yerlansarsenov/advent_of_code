9a78e30567239453d77103db31fa31f528613c95
Yerlan Sarsenov <erlan@1fit.app>
2025-12-13 14:54:34 +0500

    add day2 solution !!!

--- src/main/kotlin/day1/Main.kt
+++ src/main/kotlin/day1/Main.kt
@@ -3,6 +3,10 @@ package day1
 import java.io.File
 
 fun main() {
+    part2()
+}
+
+private fun part1() {
     val input = readInput()
         .map { Turn(it) }
     var current = 50
@@ -17,10 +21,29 @@ fun main() {
     println(zeroCounter)
 }
 
+private fun part2() {
+    val input = readInput()
+        .map { Turn2(it) }
+    var current = 50
+    var zeroCounter = 0
+    for (t in input) {
+        val (diff, zeroCount) = t.getDiffAndZeroCounts(current)
+        current = diff
+        println("curr $current")
+        println(" zero $zeroCounter")
+        zeroCounter += zeroCount
+    }
+    println(zeroCounter)
+}
+
 private fun readInput(): List<String> {
     val url = File("src/main/kotlin/day1/input")
     val rawText: String = url.readText()
     return rawText.split("\n")
+}
+
+enum class Direction {
+    L, R
 }
 
 class Turn(
@@ -39,10 +62,6 @@ class Turn(
         from.substring(1).toInt() % 100
     )
 
-    enum class Direction {
-        L, R
-    }
-
     fun getDiff(current: Int): Int {
         var diff = when (direction) {
             Direction.L -> current - step
@@ -54,3 +73,40 @@ class Turn(
         return diff % 100
     }
 }
+
+class Turn2(
+    private val direction: Direction,
+    private val step: Int
+) {
+    constructor(from: String) : this(
+        Direction.valueOf(from[0].toString()),
+        from.substring(1).toInt()
+    )
+
+    fun getDiffAndZeroCounts(current: Int): TurnResult {
+        var zeroCounts = 0
+        var res = current
+        repeat(step) {
+            res += when (direction) {
+                Direction.L -> -1
+                Direction.R -> 1
+            }
+            val zHelper = if (res < 0) res * -1 else res
+            if (zHelper % 100 == 0) {
+                zeroCounts++
+            }
+        }
+        while (res < 0) {
+            res += 100
+        }
+        return TurnResult(
+            position = res % 100,
+            zeroCounts = zeroCounts
+        )
+    }
+}
+
+data class TurnResult(
+    val position: Int,
+    val zeroCounts: Int
+)
